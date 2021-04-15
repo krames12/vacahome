9f74380b004d0d86e37688101366e3a62fac51e7
Nick Kramer <krames12@gmail.com>
2021-04-14 20:56:14 -0400

    adds dummy data

---
+++ data/houseData.js
@@ -0,0 +1,38 @@
+const houseData = [
+  {
+    city: "Detroit",
+    state: "MI",
+    price: 172,
+    bedrooms: 3,
+    bathrooms: 2,
+    squareFt: 1800,
+    description: "This fabulous Gastown is only one block from the Avenue with all the Trendy Shoppes, Fabulous Restaurants and only seconds to the Beach. South West corner, 3 bed, 2 bathroom with ocean, mountain and city views. Substantially renovated with many beautiful features, this gorgeous suite is going to knock your socks off! Just moments away you'll find all the amenities you'll need. Large eat-in Island, 9' ceilings & wood floors = unique suite potential. Depreciation report has been completed. Don't forget the in-suite laundry.",
+    owner: {
+      name: "Chris DeMars"
+    }
+  },
+  {
+    city: "Nashville",
+    state: "TN",
+    price: 229,
+    bedrooms: 2,
+    bathrooms: 1,
+    squareFt: 1344,
+    description: "Beautiful 1953 Elk Lake home ideal for the professional couple, single parent or new family. It's tastefully updated – daylight walkout lower level & separate drive in workshop, inlayed oak and fir floors and White Cabinetry/Granite counters. Depreciation Report available. Call for more details! OPEN HOUSE SAT 12-1:30.",
+    owner: {
+      name: "Raf Cevallos"
+    }
+  },
+  {
+    city: "Nashville",
+    state: "TN",
+    price: 431,
+    bedrooms: 4,
+    bathrooms: 3,
+    squareFt: 2044,
+    description: "Whether you are a first time buyer, investor, or looking for a pied de terre have a look at this one! Available for immediate occupancy for you or your tenant, this custom home stands out as a unique and refreshing offering. A traditional floorplan with 4BR/3 bath and a spacious Rec Room w/wet bar above the garage. This 'as new' home is rich with heritage features, boasting an open contemporary design with all the modern conveniences. Great floor plan with open concept design. Buses, church, shopping, all amenities close by. Covenants are in place securing the views, building style and uses in the development protecting your investment. Kitchen has eating bar w/Parking for three cars, 12mm free standing glass railings and Hand-chiseled Quartz Island. This is by far the most stunning floorplan in the entire development!",
+    owner: {
+      name: "Christian Hall"
+    }
+  },
+]
