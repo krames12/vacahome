46d238e053fa37815f32e3675018c7bdca30e6c9
Nick Kramer <krames12@gmail.com>
2021-04-24 12:05:45 -0400

    listings now update on search

--- components/ListingsGrid/index.js
+++ components/ListingsGrid/index.js
@@ -1,18 +1,13 @@
-import { useState } from "react"
 import { Container, Text } from "@chakra-ui/react"
 
 import HouseCard from "../HouseCard"
 
-const ListingsGrid = ({listings}) => {
-  const [currentListings, setCurrentListing] = useState(listings)
-
-  return (
-    <Container padding="1">
-      { currentListings.map( listing => (
-        <HouseCard {...listing} />
-      ) )}
-    </Container>
-  )
-}
+const ListingsGrid = ({listings}) => (
+  <Container padding="1">
+    { listings.map( listing => (
+      <HouseCard {...listing} />
+    ) )}
+  </Container>
+)
 
 export default ListingsGrid
